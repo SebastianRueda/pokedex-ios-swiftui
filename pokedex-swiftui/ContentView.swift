a6cf77a4c22bc44fa002fbccedc44c01e4b98d67
sebastianrueda <sebastianruedaolarte@gmail.com>
2023-08-07 17:39:07 -0300

    PokemonListView in ContentView

--- pokedex-swiftui/ContentView.swift
+++ pokedex-swiftui/ContentView.swift
@@ -2,13 +2,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        let pokemons = [
+            Pokemon(name: "name1", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"),
+            Pokemon(name: "name2", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"),
+            Pokemon(name: "name3", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"),
+            Pokemon(name: "name4", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"),
+            Pokemon(name: "name5", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"),
+            Pokemon(name: "name6", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"),
+            Pokemon(name: "name7", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"),
+            Pokemon(name: "name8", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"),
+            Pokemon(name: "name9", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"),
+            Pokemon(name: "name10", image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png")
+        ]
+        PokemonListView(pokemons: pokemons)
     }
 }
 
